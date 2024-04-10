66f8ea02dd3bdd53dd9acab0d33a5177dfbed7eb
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2024-04-10 13:47:55 -0400

    finish navbar + dropdown + google sign in/signout

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: ["lh3.googleusercontent.com", "res.cloudinary.com", "images.unsplash.com", "cdn.pixabay.com", "cdn.statically.io"]
+    }
+};
 
 export default nextConfig;
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -2,17 +2,18 @@ import { getAuthSession } from "@/lib/nextauth";
 import Link from "next/link";
 import React from "react";
 import SignInButton from "./SignInButton";
+import UserAccountNavbar from "./UserAccountNavbar";
 
 type Props = {};
 
 const Navbar = async (props: Props) => {
   const session = await getAuthSession();
 
-  if (session?.user) {
-    return <pre>{JSON.stringify(session.user, null, 2)}</pre>;
-  } else {
-    return <div>Not Logged In</div>;
-  }
+  // if (session?.user) {
+  //   return <pre>{JSON.stringify(session.user, null, 2)}</pre>;
+  // } else {
+  //   return <div>Not Logged In</div>;
+  // }
 
   return (
     <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
@@ -24,7 +25,11 @@ const Navbar = async (props: Props) => {
           </p>
         </Link>
         <div className="flex items-center">
-          <SignInButton text="Sign In" />
+          {session?.user ? (
+            <UserAccountNavbar user={session.user} />
+          ) : (
+            <SignInButton text="Sign In" />
+          )}
         </div>
       </div>
     </div>
