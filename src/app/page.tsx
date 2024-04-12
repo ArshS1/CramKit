3a92ecef8f5cbc4c42ce7b856b1f8518a6420994
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2024-04-12 14:41:50 -0400

    setup home page login/dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
-import { Button } from "@/components/ui/button";
-import { prisma } from "@/lib/db";
+import SignInButton from "@/components/ui/SignInButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
 
+export default async function Home() {
+  const session = await getAuthSession();
 
-export default function Home() {
-
-  return (
-    <Button>Home Page</Button>
-  )
-
+  if (session?.user) {
+    return redirect("/dashboard");
+  } else {
+    return (
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Card className="w-[300px">
+          <CardHeader>
+            <CardTitle>Welcome To CramKit</CardTitle>
+            <CardDescription>
+              CramKit is a flashcard app that helps you study and learn more
+              effectively.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <SignInButton text="Sign in with Google" />
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 }
