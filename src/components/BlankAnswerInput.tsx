3bd8d884d2596f77573bce01c2c1ce807d8c2151
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2024-04-24 14:11:47 -0400

    add blanks for input -- open ended

--- src/components/BlankAnswerInput.tsx
+++ src/components/BlankAnswerInput.tsx
@@ -5,19 +5,41 @@ type Props = {
   answer: string;
 };
 
+const BLANKS = "-----";
+
 const BlankAnswerInput = ({ answer }: Props) => {
-    const keywords = React.useMemo(() => {
-        return keywordExtractor.extract(answer, {
-            language: "english",
-            remove_digits: true,
-            return_changed_case: false,
-            remove_duplicates: false,
-        });
-    }, []);
+  const keywords = React.useMemo(() => {
+    const words = keywordExtractor.extract(answer, {
+      language: "english",
+      remove_digits: true,
+      return_changed_case: false,
+      remove_duplicates: false,
+    });
+    const shuffleWords = words.sort(() => Math.random() - 0.5);
+    return shuffleWords.slice(0, 2);
+  }, [answer]);
+
+  const insertBlanks = React.useMemo(() => {
+    const answerWithBlanks = keywords.reduce((acc, keyword) => {
+      return acc.replace(keyword, BLANKS);
+    }, answer);
+    return answerWithBlanks;
+  }, [answer, keywords]);
 
   return (
     <div className="flex justify-start w-full mt-4">
-      <h1 className="text- xl font-semibold"></h1>
+      <h1 className="text- xl font-semibold">
+        {insertBlanks.split(BLANKS).map((word, index) => (
+          <>
+            {word}
+            <input
+              type="text"
+              id="user-blank-input"
+              className="text-center border-b-2 border-black dark:border-white w-28 focus:border-b-4 focus:outline-none "
+            />
+          </>
+        ))}
+      </h1>
     </div>
   );
 };
